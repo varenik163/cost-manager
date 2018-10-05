bae60db56cd81fa6e06fe9b3124b35f7ac88a592
varenik163 <varenik163@yandex.ru>
2018-10-05 12:34:53 +0400

    init

---
+++ src/components/YMViewer/index.js
@@ -0,0 +1,34 @@
+import React, {Component, Fragment} from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	Button,
+	Image,
+	ImageBackground,
+	DrawerLayoutAndroid,
+	Picker,
+	CameraRoll
+} from 'react-native';
+
+class YMViewer extends Component {
+
+	componentDidMount() {
+
+	}
+
+	render() {
+		const {list} = this.props;
+
+		return <Fragment>
+
+		</Fragment>
+	}
+}
+
+YMViewer.propTypes = {};
+
+export default connect(state => ({}), {})(YMViewer)
